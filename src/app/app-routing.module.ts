b27f0d5db9d682ee801d7f5295fdb58dc78b59a0
Ee-Chee <eechee.leng@gmail.com>
2020-09-03 01:43:50 +0200

    Add flex layout, Angular-mat and responsive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { UsersListComponent } from './components/users-list/users-list.component';
+import { UserManagementComponent } from './components/user-management/user-management.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: UsersListComponent, pathMatch: 'full' },
+    { path: 'registration', component: UserManagementComponent },
+    { path: 'person/:id', component: UserManagementComponent },
+    { path: '**', redirectTo: '' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(routes)
+    ],
+    exports: [
+        RouterModule
+    ]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+
+import { UsersListComponent } from './components/users-list/users-list.component';
+import { UserManagementComponent } from './components/user-management/user-management.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SideNavigationComponent } from './components/side-navigation/side-navigation.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        UsersListComponent,
+        UserManagementComponent,
+        LayoutComponent,
+        HeaderComponent,
+        SideNavigationComponent
+    ],
+    imports:
+        [
+            BrowserModule,
+            AppRoutingModule,
+            BrowserAnimationsModule,
+            FormsModule,
+            ReactiveFormsModule,
+            FlexLayoutModule,
+            MatToolbarModule,
+            MatButtonToggleModule,
+            MatSidenavModule,
+            MatButtonModule,
+            MatIconModule,
+            MatListModule
+        ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
